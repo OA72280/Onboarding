0e58cb2d93831ebc5f38da47393c7cf264b01fac
Kyle Brown <brow1144@purdue.edu>
2019-05-31 11:28:53 -0400

    And again...

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ class App extends Component {
     return (
       <Switch>
 
-        <Route exact path='/Home' render={(match) => (
+        <Route exact path={process.env.PUBLIC_URL + '/Home'} render={(match) => (
           this.signedIn()
             ? (this.state.user
               ?
@@ -147,10 +147,10 @@ class App extends Component {
                null
             )
             // eslint-disable-next-line
-            : <Redirect to="/SignIn"/>
+            : <Redirect to={process.env.PUBLIC_URL + '/SignIn'}/>
         )}/>
 
-        <Route exact path='/Calendar' render={() => (
+        <Route exact path={process.env.PUBLIC_URL + '/Calendar'} render={() => (
           this.signedIn()
             ? (this.state.user
               ?
@@ -159,10 +159,10 @@ class App extends Component {
                null
             )
             // eslint-disable-next-line
-            : <Redirect to="/SignIn"/>
+            : <Redirect to={process.env.PUBLIC_URL + '/SignIn'}/>
         )}/>
 
-        <Route exact path='/Mentors' render={() => (
+        <Route exact path={process.env.PUBLIC_URL + '/Mentors'} render={() => (
           this.signedIn()
             ? (this.state.user
               ?
@@ -171,27 +171,27 @@ class App extends Component {
                null
             )
             // eslint-disable-next-line
-            : <Redirect to="/SignIn"/>
+            : <Redirect to={process.env.PUBLIC_URL + '/SignIn'}/>
         )}/>
 
-        <Route exact path='/SignIn' render={() => (
+        <Route exact path={process.env.PUBLIC_URL + '/SignIn'} render={() => (
           !this.signedIn()
             ? <SignIn/>
             // eslint-disable-next-line
-            : <Redirect to={`/Home`}/>
+            : <Redirect to={process.env.PUBLIC_URL + '/Home'}/>
         )}/>
         
-        <Route exact path='/CreateAccount' render={() => (
+        <Route exact path={process.env.PUBLIC_URL + '/CreateAccount'} render={() => (
           !this.signedIn()
             ? <CreateUser {...methods} />
             // eslint-disable-next-line
-            : <Redirect to={`/Home`}/>
+            : <Redirect to={process.env.PUBLIC_URL + '/Home'}/>
         )}/>
 
         <Route render={() => {
           return (
             // eslint-disable-next-line
-            <Redirect to={`/Home`} />
+            <Redirect to={process.env.PUBLIC_URL + '/Home'} />
           )
         }}/>
 
